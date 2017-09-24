6fdad2a84bb8d8f31fe2cfaa90a1f6fc354f1d71
Bin Ichiki <bix@cyclont.toybox>
2017-09-24 23:33:15 +0900

    change class

--- curtask.psm1
+++ curtask.psm1
@@ -1,14 +1,13 @@
 
-class CurTask_GenParam {
-}
-
 
 class CurTask {
     [int] $taskId;
+    [string] $taskLabel;
     [int] $interval;
     [int] $timeout;
-    CurTask([int] $taskId,[int] $interval,[int] $timeout) {
+    CurTask([int] $taskId,[string] $taskLabel,[int] $interval,[int] $timeout) {
         $this.taskId = $taskId;
+        $this.taskLabel = $taskLabel;
         $this.interval = $interval;
         $this.timeout = $timeout;
     }
@@ -16,18 +15,67 @@ class CurTask {
 class CurTaskTcp : CurTask {
     [string] $hostname;
     [int] $port;
-    CurTaskTcp([int] $taskId,[int] $interval,[int] $timeout,[string] $hostname,[int] $port) :
-        CurTask($taskId,$interval,$timeout)    {
+    CurTaskTcp([int] $taskId,[string] $taskLabel,[int] $interval,[int] $timeout,[string] $hostname,[int] $port) : base($taskId,$taskLabel,$interval,$timeout)    {
         $this.hostname=$hostname;
         $this.port=$port;
     }
 }
 
-
 class CurTaskList {
-     [CurTask[]] $tasklist;
-     addTask([CurTask] $obj) {
-     }
+    [CurTask[]] $tasklist;
+    addTask([CurTask] $obj) {
+        $this.tasklist+=$obj;
+    }
+    parseXml([string] $xmlfilename) {
+        $xml=[xml](Get-Content $xmlfilename);
+        $xml.GetElementsByTagName("TASK") | foreach -process {
+            [int] $taskId;
+            [string] $taskLabel;
+            [int] $interval;
+            [int] $timeout;
+            
+            [string] $hostname;
+            [int] $port;
+            [CurTaskTcp] $taskTcp;
+            
+            $task=$_;
+            $task.ChildNodes.GetEnumerator()  |  foreach -process {
+                if ($_.LocalName.CompareTo("GEN_PARAM")-eq 0) {
+                    $gen_params = $_;
+                    $gen_params.ChildNodes.GetEnumerator()  |  foreach -process {
+                        $gen_param=$_; 
+                        if ($gen_param.LocalName.CompareTo("TASKID") -eq 0) {
+                            $taskId = $gen_param.InnerText;
+                        } elseif ($gen_param.LocalName.CompareTo("TASKLABEL") -eq 0) {
+                            $taskLabel = $gen_param.InnerText;
+                        } elseif ($gen_param.LocalName.CompareTo("INTERVAL") -eq 0) {
+                            $interval = $gen_param.InnerText;
+                        } elseif ($gen_param.LocalName.CompareTo("TIMEOUT") -eq 0) {
+                            $timeout = $gen_param.InnerText;
+                        }
+                    }
+                } elseif ($_.LocalName.CompareTo("SERV_PARAM") -eq 0) {
+                    $serv_params = $_;
+                    $serv_params.ChildNodes.GetEnumerator()  |  foreach -process {
+                        if ($_.LocalName.CompareTo("TCP") -eq 0) {
+                            $tcp_params = $_;
+                            $tcp_params.ChildNodes.GetEnumerator()  |  foreach -process {
+                                $tcp_param = $_;
+                                if ($tcp_param.LocalName.CompareTo("HOSTNAME") -eq 0) {
+                                    $hostname = $tcp_param.InnerText;
+                                } elseif ($tcp_param.LocalName.CompareTo("PORT") -eq 0) {
+                                    $port = $tcp_param.InnerText;
+                                }
+                            }
+                        }
+                    }
+                    $taskTcp = [CurTaskTcp]::new($taskId,$taskLabel,$interval,$timeout,
+                                                $hostname,$port);
+                    $this.addTask($taskTcp);
+                }
+            }
+        }
+    }
 }
 
 
--- curtasklist.ps1
+++ curtasklist.ps1
@@ -4,40 +4,9 @@ Import-Module .\curtask.psm1 -Force
 
 $filename="curtask.xml";
 
-$xml=[xml](Get-Content $filename);
+
+$tasklist=[CurTaskList]::New();
+$tasklist.parseXml("$filename");
 
 
-# Write-Output ($xml.GetElementsByTagName("TASKLIST")).GetElementsByTagName("GEN_PARAM")
-
-
-$gen_params=($xml.GetElementsByTagName("TASKLIST")).GetElementsByTagName("GEN_PARAM");
-
-# çÄñ⁄ñºóÒãì
-
-$title="";
-
-$ienum=$gen_params.Get(0).GetEnumerator();
-while($ienum.MoveNext()) {
-    $data=$ienum.Current;
-    $title +=$data.LocalName;
-    $title +=" ";
-}
-
-Write-Output $title;
-
-$gen_params | foreach -process {
-    $gen_param=$_;
-    $dataline="";
-    $gen_param.ChildNodes |  foreach -process {
-
-    $dataline +=$_.InnerText;
-    $dataline +=" ";
-    }
-    Write-Output $dataline;
-}
-
-
-write-func
-
-$o=[CurTask_GenParam]::New();
-
+$tasklist.tasklist.GetEnumerator() 
